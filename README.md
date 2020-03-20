1b7aa87f297ab4530d1b0750d1206f0c6a170618
EC2 Default User <ec2-user@ip-172-31-16-207.ec2.internal>
2020-03-20 16:04:52 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,90 @@
+# Native Secrets 
+
+This repo is a proof-of-concept (PoC) showing how native Kubernetes secrets can be support via AWS Secrets Manager. The basic idea of the PoC is to use an extension point of the Kubernetes API server called [dynamic admission control](https://kubernetes.io/docs/reference/access-authn-authz/extensible-admission-controllers/): when a user creates a secret, a mutating Webhook (implemented as an AWS Lambda function) intercepts the process of persisting the payload into `etcd` and replaces it with the ARN of a secret managed by the [AWS Secrets Manager](https://aws.amazon.com/secrets-manager/).
+
+## Installation
+
+In order to build and deploy the service, clone this repo and make sure you've got the following available, locally:
+
+- The [aws](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-install.html) CLI
+- The [SAM CLI](https://github.com/awslabs/aws-sam-cli)
+- Go 1.12 or above
+- A Kubernetes 1.14 cluster or above with `kubectl` configured, locally
+
+Additionally, I recommend that you have [jq](https://stedolan.github.io/jq/download/) installed.
+
+First, prepare the S3 bucket for the Lambda function that provides the webhook (make sure that you pick different name for the `WEBHOOK_BUCKET` bucket env variable since buckets need to be globally unique):
+
+```sh
+export WEBHOOK_BUCKET=nase-webhook
+
+aws s3api create-bucket \
+          --bucket $WEBHOOK_BUCKET \
+          --create-bucket-configuration LocationConstraint=$(aws configure get region) \
+          --region $(aws configure get region)
+```
+
+Now, to install the webhook, execute:
+
+```sh
+make deploy
+``` 
+
+To verify if all went well, compare the output of the following command with your own output:
+
+```sh
+$ kubectl describe mutatingwebhookconfigurations/nase
+Name:         nase
+Namespace:
+Labels:       <none>
+Annotations:  kubectl.kubernetes.io/last-applied-configuration:
+                {"apiVersion":"admissionregistration.k8s.io/v1beta1","kind":"MutatingWebhookConfiguration","metadata":{"annotations":{},"name":"nase"},"we...
+API Version:  admissionregistration.k8s.io/v1beta1
+Kind:         MutatingWebhookConfiguration
+Metadata:
+  Creation Timestamp:  2019-12-12T10:36:57Z
+  Generation:          1
+  Resource Version:    2394053
+  Self Link:           /apis/admissionregistration.k8s.io/v1beta1/mutatingwebhookconfigurations/nase
+  UID:                 5277a118-1ccb-11ea-98a5-0a084944784a
+Webhooks:
+  Admission Review Versions:
+    v1beta1
+  Client Config:
+    Ca Bundle:     ***
+    URL:           https://******.execute-api.us-west-2.amazonaws.com/Prod/webhook
+  Failure Policy:  Fail
+  Name:            nase.mhausenblas.info
+  Namespace Selector:
+  Rules:
+    API Groups:
+
+    API Versions:
+      v1
+    Operations:
+      CREATE
+      UPDATE
+    Resources:
+      secrets
+    Scope:          *
+  Side Effects:     Unknown
+  Timeout Seconds:  30
+Events:             <none>
+```
+
+You're now ready to use the demo. 
+
+Notes:
+
+- The CA bundle used in the [webhook config](webhook-config-template.yaml) comes from [Amazon Trust Services](https://www.amazontrust.com/repository/).
+- This is a PoC, not a production-ready setup. In order to lock down the webhook, that is, make sure that it can only be called from your Kubernetes cluster, you'd need to [restrict the API Gateway](https://aws.amazon.com/blogs/compute/introducing-amazon-api-gateway-private-endpoints/) access to its VPC.
+
+## Usage
+
+```sh
+$ kubectl create secret generic somesecret --from-literal=nase=supersecret
+secret/somesecret created
+
+$ kubectl get secret somesecret -o "jsonpath={.data['nase']}" | base64 -D
+arn:aws:secretsmanager:us-west-2:123456789012:secret:.nase-dzWHWN
+```
---
+++ pods/webhook/main.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+
+	"github.com/appscode/jsonpatch"
+	admissionv1beta1 "k8s.io/api/admission/v1beta1"
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/secretsmanager"
+)
+
+func serverError(err error) (events.APIGatewayProxyResponse, error) {
+	fmt.Println(err.Error())
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusInternalServerError,
+		Headers: map[string]string{
+			"Access-Control-Allow-Origin": "*",
+		},
+		Body: fmt.Sprintf("%v", err.Error()),
+	}, nil
+}
+
+func responseAdmissionReview(review *admissionv1beta1.AdmissionReview) (events.APIGatewayProxyResponse, error) {
+	reviewjson, err := json.Marshal(review)
+	if err != nil {
+		return serverError(fmt.Errorf("Unexpected decoding error: %v", err))
+	}
+	fmt.Println(string(reviewjson))
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Headers: map[string]string{
+			"Access-Control-Allow-Origin": "*",
+			"Content-Type":                "application/json",
+		},
+		Body: string(reviewjson),
+	}, nil
+}
+
+func genCodec() serializer.CodecFactory {
+	scheme := runtime.NewScheme()
+	scheme.AddKnownTypes(schema.GroupVersion{Group: "apps", Version: "v1"}, &appsv1.Deployment{})
+	codecs := serializer.NewCodecFactory(scheme)
+	_ = runtime.ObjectDefaulter(scheme)
+	// fmt.Printf("DEBUG:: SCHEME\n %v\n", scheme)
+	return codecs
+}
+
+func retrieveSecret(ns, secretARN string) (string, error) {
+	fmt.Printf("DEBUG:: trying to retrieve secret with ARN %v\n", secretARN)
+	svc := secretsmanager.New(session.New())
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId: aws.String(secretARN),
+	}
+	result, err := svc.GetSecretValue(input)
+	if err != nil {
+		return "", err
+	}
+	return *result.SecretString, nil
+}
+
+func mutate(body string) (events.APIGatewayProxyResponse, error) {
+	codecs := genCodec()
+	reviewGVK := admissionv1beta1.SchemeGroupVersion.WithKind("AdmissionReview")
+	obj, gvk, err := codecs.UniversalDeserializer().Decode([]byte(body), &reviewGVK, &admissionv1beta1.AdmissionReview{})
+	if err != nil {
+		return serverError(fmt.Errorf("Can't decode body: %v", err))
+	}
+	review, ok := obj.(*admissionv1beta1.AdmissionReview)
+	if !ok {
+		serverError(fmt.Errorf("Unexpected GroupVersionKind: %s", gvk))
+	}
+	if review.Request == nil {
+		return serverError(fmt.Errorf("Unexpected nil request"))
+	}
+	review.Response = &admissionv1beta1.AdmissionResponse{
+		UID: review.Request.UID,
+	}
+	if review.Request.Object.Object == nil {
+		var err error
+		review.Request.Object.Object, _, err = codecs.UniversalDeserializer().Decode(review.Request.Object.Raw, nil, nil)
+		if err != nil {
+			review.Response.Result = &metav1.Status{
+				Message: err.Error(),
+				Status:  metav1.StatusFailure,
+			}
+			return responseAdmissionReview(review)
+		}
+	}
+	original := review.Request.Object.Raw
+	ns := review.Request.Namespace
+	var bd []byte
+	switch deploy := review.Request.Object.Object.(type) {
+	case *appsv1.Deployment:
+		fmt.Printf("DEBUG:: DEPLOYMENT in namespace %v\n%v\n", ns, deploy)
+		// targetsec := deploy.Spec.Template.Spec.Volumes[0].Secret.SecretName
+		secarn, ok := deploy.Annotations["nase.mhausenblas.info/secret-arn"]
+		if ok {
+			plaintext, err := retrieveSecret(ns, secarn)
+			if err != nil {
+				return serverError(fmt.Errorf("Can't retrieve secret from Secret Manager: %v", err))
+			}
+			fmt.Printf("DEBUG:: PLAINTEXT %v\n", plaintext)
+		}
+		bd, err = json.Marshal(deploy)
+		if err != nil {
+			return serverError(fmt.Errorf("Unexpected encoding error: %v", err))
+		}
+	default:
+		review.Response.Result = &metav1.Status{
+			Message: fmt.Sprintf("Unexpected type %T", review.Request.Object.Object),
+			Status:  metav1.StatusFailure,
+		}
+		return responseAdmissionReview(review)
+	}
+	ops, err := jsonpatch.CreatePatch(original, bd)
+	if err != nil {
+		return serverError(fmt.Errorf("Unexpected diff error: %v", err))
+	}
+	review.Response.Patch, err = json.Marshal(ops)
+	if err != nil {
+		return serverError(fmt.Errorf("Unexpected patch encoding error: %v", err))
+	}
+	typ := admissionv1beta1.PatchTypeJSONPatch
+	review.Response.PatchType = &typ
+	review.Response.Allowed = true
+	return responseAdmissionReview(review)
+}
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Printf("DEBUG:: pods webhook start\n")
+	response, err := mutate(request.Body)
+	if err != nil {
+		return serverError(err)
+	}
+	fmt.Printf("DEBUG:: pods webhook done\n")
+	return response, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
